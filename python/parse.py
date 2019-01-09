429c5b2fba95848cd9ebce764e83c910ef14cab4
MrEnigmamgine <mayes.joshua.d@gmail.com>
2019-01-08 23:05:03 -0500

    Finished
    The files needed some human tlc at the end so it wasn't perfect, but was very efficient.

--- python/parse.py
+++ python/parse.py
@@ -1,53 +1,102 @@
 import xml.etree.ElementTree as ET  
+import time
 xmlp = ET.XMLParser(encoding='ISO-8859-1')
 
 tree= ET.parse('db.xml',parser=xmlp)
 root = tree.getroot()
 npcdata = root.find('reference/npcdata')
 
-if(npcdata.find('balhannoth')):
-    for npc in npcdata:
-        if(npc.find('name')):
-            name = npc.find('name').text
-            print(name)
-        npctype = npc.find('type').text # I think "type" is a reserved word
-        size = npc.find('size').text
-        alignment = npc.find('alignment').text
-        ac = npc.find('ac').text
+for npc in npcdata:
+    file= open('./test/'+npc.tag+'.markdown','w+')
+    name = npc.find('name').text        
+    npctype = npc.find('type').text # I think "type" is a reserved word
+    size = str(npc.find('size').text)
+    alignment = npc.find('alignment').text
+    ac = str(npc.find('ac').text)
+    actext = '' #Element always exists, but is not always populated with data
+    if(not npc.find('actext').text is None):
         actext = npc.find('actext').text
-        hp = npc.find('hp').text
-        hd = npc.find('hd').text
-        speed = npc.find('speed').text
-        strength = npc.find('abilities/strength/score').text
-        strengthmod = npc.find('abilities/strength/bonus').text
-        dexterity = npc.find('abilities/dexterity/score').text
-        dexteritymod = npc.find('abilities/dexterity/bonus').text
-        constitution = npc.find('abilities/constitution/score').text
-        constitutionmod = npc.find('abilities/constitution/bonus').text
-        wisdom = npc.find('abilities/wisdom/score').text
-        wisdommod = npc.find('abilities/wisdom/bonus').text
-        charisma = npc.find('abilities/charisma/score').text
-        charismamod = npc.find('abilities/charisma/bonus').text
-        print(npc.find('skills'),npc)
-        if(npc.find('skills')):
-            print("I found it!")
-    # savingthrows = npc.find('savingthrows').text
-    #  skills = npc.find('skills').text
-    #  conditionimmunities = npc.find('conditionimmunities').text
-        senses = npc.find('senses').text
-    #  languages = npc.find('languages').text
-        cr = npc.find('cr').text
-        xp = npc.find('xp').text
+    hp = npc.find('hp').text
+    hd = npc.find('hd').text
+    speed = npc.find('speed').text
 
-        
+    def addplus(string): #Check if the modifier is above zero and if so add a plus sign to it
+        num = int(string)
+        return "+"+string if num > 0 else string
 
+    strength = npc.find('abilities/strength/score').text #I think 'str' is reserved
+    strmod = addplus(npc.find('abilities/strength/bonus').text)
+    dex = npc.find('abilities/dexterity/score').text
+    dexmod = addplus(npc.find('abilities/dexterity/bonus').text)
+    con = npc.find('abilities/constitution/score').text
+    conmod = addplus(npc.find('abilities/constitution/bonus').text)
+    intel = npc.find('abilities/intelligence/score').text
+    intmod = addplus(npc.find('abilities/intelligence/bonus').text)
+    wis = npc.find('abilities/wisdom/score').text
+    wismod = addplus(npc.find('abilities/wisdom/bonus').text)
+    cha = npc.find('abilities/charisma/score').text
+    chamod = addplus(npc.find('abilities/charisma/bonus').text)
+    cr = npc.find('cr').text
+    senses = npc.find('senses').text
+    languages = npc.find('languages').text
+    xp = npc.find('xp').text
 
-#for child in root:
-#        print(att.tag, att.value)
-#    for att in child.iter('name'):
-#/
+    file.write(
+        '---\nlayout: post\n'+
+        'title: "'+name+'"\n'+
+        'date: '+time.strftime("%Y-%m-%d")+'\n'+
+        'tags: ['+size.lower()+', '+npctype.split(' ')[0]+', cr'+cr+', mordenkainens-tome-of-foes]\n'+
+        'page_number: 999\n'+
+        '---\n\n'+
+        '**'+size+' '+npctype+', '+alignment+'**\n\n'+
+        '**Armor Class** '+ac+' '+actext+'\n\n'+
+        '**Hit Points** '+hp+'  '+hd+'\n\n'+
+        '**Speed** '+speed+'\n\n'+
+        '|   STR   |   DEX   |   CON   |   INT   |   WIS   |   CHA   |\n'+
+        '|:-------:|:-------:|:-------:|:-------:|:-------:|:-------:|\n'+
+        '| '+strength+' ('+strmod+') | '+dex+' ('+dexmod+') | '+con+' ('+conmod+') | '+intel+' ('+intmod+') | '+wis+' ('+wismod+') | '+cha+' ('+chamod+') |\n\n'
+        )
 
-#print(root[0].find('name').text)
-#for actions in root[0].find('actions'):
-#    print(actions.find('name').text)
-#    print(actions.find('desc').text)
+    if(not npc.find('savingthrows') is None):   
+        savingthrows = npc.find('savingthrows').text
+        file.write('**Saving Throws** '+savingthrows+'\n\n')
+    if(not npc.find('skills') is None):
+        skills = npc.find('skills').text
+        file.write('**Skills** '+skills+'\n\n')
+    if(not npc.find('damageresistances') is None):
+        damageresistances = npc.find('damageresistances').text
+        file.write('**Damage Resistances** '+damageresistances+'\n\n')
+    if(not npc.find('damageimmunities') is None):
+        damageimmunities = npc.find('damageimmunities').text
+        file.write('**Damage Immunities** '+damageimmunities+'\n\n')
+    if(not npc.find('conditionimmunities') is None):
+        conditionimmunities = npc.find('conditionimmunities').text
+        file.write('**Condition Immunities** '+conditionimmunities+'\n\n')
+
+    file.write(
+        '**Senses** '+senses+'\n\n'+
+        '**Languages** '+languages+'\n\n'+
+        '**Challenge** '+cr+' ('+xp+' XP)'
+    )
+    
+    if(not npc.find('traits') is None):
+        for child in npc.find('traits'):
+            childname = str(child.find('name').text)
+            childdesc = str(child.find('desc').text)
+            file.write('\n\n***'+childname+'.*** '+childdesc)
+    if(not npc.find('actions') is None):
+        file.write('\n\n**Actions**')
+        for child in npc.find('actions'):
+            childname = str(child.find('name').text)
+            childdesc = str(child.find('desc').text)
+            file.write('\n\n***'+childname+'*** '+childdesc)
+    if(not npc.find('legendaryactions') is None):
+        file.write('\n\n**Legendary Actions**')
+        for child in npc.find('legendaryactions'):
+            childname = str(child.find('name').text)
+            childdesc = str(child.find('desc').text)
+            if(childname == 'Options'):
+                file.write('\n\n'+childdesc)
+            else:
+                file.write('\n\n***'+childname+'*** '+childdesc)
+    file.close()
